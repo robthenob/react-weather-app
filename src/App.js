26d38bdaa27cdbae63c525c7a883aeeb25510800
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-05 19:15:33 -0700

    Added GItHub repo URLin footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>Weather App</p>
+      <h1>Weather App</h1>
+      <footer>
+        This website was coded by Randi Bradley and is{" "}
+        <a
+          href="https://github.com/robthenob/react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
