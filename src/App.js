c6254ec45a0bf42be5957c918433c830492195da
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-05 20:33:55 -0700

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This website was coded by Randi Bradley and is{" "}
-        <a
-          href="https://github.com/robthenob/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This website was coded by Randi Bradley and is{" "}
+          <a
+            href="https://github.com/robthenob/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
